73220c118f31164e5d16ecaa60b3675d7dc0927b
raimundogomes <netoufc@gmail.com>
2016-05-29 17:56:18 -0300

    teste

---
+++ SGR/app/src/main/java/com/imd030/sgr/builder/ExamesBulder.java
@@ -0,0 +1,35 @@
+package com.imd030.sgr.builder;
+
+import com.imd030.sgr.entiitys.Exame;
+import com.imd030.sgr.entiitys.TipoExame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Neto on 29/05/2016.
+ */
+public class ExamesBulder {
+
+    public List<Exame> getListaExames(){
+
+        return new ArrayList<Exame>();
+
+    }
+
+    public Exame adicionaExameSangue( ){
+       return  adicionaExame(TipoExame.SANGUE);
+
+    }
+
+    public Exame adicionaExameUrina(){
+        return  adicionaExame( TipoExame.URINA);
+
+    }
+
+    public Exame adicionaExame(TipoExame tipo ){
+
+       return new Exame(tipo);
+
+    }
+}
